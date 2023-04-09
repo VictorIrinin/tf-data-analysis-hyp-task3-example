70858009d1fb9cce4ea86803440c0c5153654a9c
VictorIrinin <129628121+VictorIrinin@users.noreply.github.com>
2023-04-09 23:05:42 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,8 +4,14 @@ import numpy as np
 
 chat_id = 5780444792 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    res = False
+    s = np.median(x)
+    if s >= 500:
+        res = True
+    else:
+        res = False
+    return res # Ваш ответ, True или False
